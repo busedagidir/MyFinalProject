82c69f8897fd6f436bd90364edf1729b4a36c606
Buse Dağıdır <buse.dagidir@ogr.iu.edu.tr>
2021-02-03 23:47:25 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepo.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class : referans tip
+    //IEntity : IEntity olabilri veya IEntity implemente eden bir nesne olabilri
+    public interface IEntityRepo<T> where T:class, IEntity  //bana calisacagin tipi soyle
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);  //eticaret sitelerinde filtreleme yapmani saglayan kod
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+
+    //interface metodları default publictir
+    public interface IProductDal : IEntityRepo<Product>
+    {
+        
+    }
+}
